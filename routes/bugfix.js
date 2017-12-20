fa50a6c5064f2170501414d243b2b4196d3334a0
logpacket <199sd@naver.com>
2017-12-20 23:41:59 +0900

    i fuck it

---
+++ routes/bugfix.js
@@ -0,0 +1,21 @@
+var router=express.Router();
+var model = require('./model');
+var filter = require('./filtering');
+router.get('/', function(req, res) {
+    res.render('fuckkkkkkkkkkkkkk');
+}).post('/',function(){
+    model.find(function(err,result){
+        if(err){
+            console.log("fucking err in bugfix");
+            throw err;
+        }
+        if(result){
+            for(var i in result){
+                result[i].update({score:0});
+            }
+
+        }
+    });
+});
+
+module.exports = router;

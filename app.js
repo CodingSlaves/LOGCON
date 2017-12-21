b302ba4677b9683cfddec0e034aee9bb9bdf4d73
logpacket <199sd@naver.com>
2017-12-21 22:37:04 +0900

    session score

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('404',{message:err.status});
 });
 
 module.exports = app;

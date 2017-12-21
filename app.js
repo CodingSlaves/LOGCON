ce160b89ab5f12b501ab3269375d2b83ca47e4cb
logpacket <199sd@naver.com>
2017-12-21 21:31:32 +0900

    session score

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
-app.use(logger('dev',{stream:accessLogStream}));
+app.use(logger('combined',{stream:accessLogStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());

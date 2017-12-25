1510c8ee39d2a26dab8da26de115b3894412c704
logpacket <199sd@naver.com>
2017-12-25 19:57:14 +0900

    으악

--- routes/Ranking.js
+++ routes/Ranking.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var model = require('./model');
 
+var pscore=[300,300,300,500,200,100,400,300,100,300,200,500,100,100,100,700,0,0,0,0,0];
+
 router.get('/high',function(req,res){
     model.find({grade:true}).sort({score:-1}).exec(function(err,result){
         if(err){
@@ -20,6 +22,19 @@ router.get('/high',function(req,res){
             res.render('ranking',{users:result});
         }
     });
+}).get('/',function(req,res){
+   model.find({grade:false},function(err,result){
+      for(var i in result){
+          var score = 0;
+          for(var k in result[i].problems){
+              if(result[i].problems[k] === true){
+                    score += pscore[k];
+              }
+          }
+          console.log(result[i].nickname+':'+score);
+      }
+   });
+    res.redirect('/ranking/middle');
 });
 
 module.exports = router;
